61d9f4f61d3631c655de4f5e35ae0f1bdf07f1ac
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-12-08 22:44:27 +0900

    expressのsample

--- backend/Express/src/server/index.ts
+++ backend/Express/src/server/index.ts
@@ -1,15 +1,35 @@
 import Express from 'express';
+import cors from 'cors';
+import { Health } from '../types/api';
 
 const app = Express();
 
+// CORS対応
+app.use(cors());
+
+// Router
 app.get('/', (req, res) => {
   const data = { message: 'pong' };
   res.send(data);
 });
+app.get('/api/health', (req, res) => {
+  const data: Health = { message: 'health' };
+  res.send(data);
+});
+// routeに一致しないRequest
+app.use((req, res, next) => {
+  res.sendStatus(404);
+  next({ statusCode: 404 });
+});
+// Error Route
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: { statusCode: number }, req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  console.log(err.statusCode);
+});
 
+// Express Serverの起動
 const port = 8888;
 const host = 'localhost';
-
 app.listen(port, host, () => {
   console.log(`Runnig on http://${host}:${port}`);
 });
